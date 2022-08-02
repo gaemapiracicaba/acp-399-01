deac9922755381e7b6fa227d10f47a175fc2c503
Michel Metran <michelmetran@gmail.com>
2022-08-02 12:19:14 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 project_path = Path('.').resolve().parent
-print(project_path)
+print(f'Pasta do Projeto é: {project_path}')
 
 data_path = project_path / 'data'
 input_path = data_path / 'input'
@@ -16,23 +16,10 @@ output_path = data_path / 'output'
 
 temp_path = input_path / 'temp'
 maps_path = output_path / 'maps'
-shps_path = output_path / 'shps'
 geo_path = output_path / 'geo'
 
 input_path.mkdir(exist_ok=True)
 output_path.mkdir(exist_ok=True)
 temp_path.mkdir(exist_ok=True)
 maps_path.mkdir(exist_ok=True)
-shps_path.mkdir(exist_ok=True)
 geo_path.mkdir(exist_ok=True)
-
-
-# import os
-# import shutil
-
-#shutil.rmtree(temp_path, ignore_errors=True)
-#shutil.rmtree(output_path, ignore_errors=True)
-
-#os.makedirs(output_path, exist_ok=True)
-#os.makedirs(temp_path, exist_ok=True)
-#os.makedirs(maps_path, exist_ok=True)
